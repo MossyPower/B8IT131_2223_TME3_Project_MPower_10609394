82599664e42c8bc271407766678bd508e9fdb559
MossyPower <10609394@mydbs.ie>
2023-11-13 21:09:24 +0000

    Players Mappings tweeks

--- MvcRugby/Models/Mappings/Players/P_Summaries/PS_SportEventStatus/PSS_PeriodScores/PSS_PeriodScores.cs
+++ MvcRugby/Models/Mappings/Players/P_Summaries/PS_SportEventStatus/PSS_PeriodScores/PSS_PeriodScores.cs
@@ -1,11 +1,13 @@
 // Naming Convention Meaning: 
 // PSSS = Players | Summaries | Sport Event Status 
 
-namespace MvcRugby.Mappings;
-public class PSS_PeriodScores
+namespace MvcRugby.Mappings
 {
-    public string? home_score { get; set; } // 6
-    public string? away_score { get; set; } // 10
-    public int? type { get; set; } // "regular_period"
-    public int? number { get; set; } // 1
+    public class PSS_PeriodScores
+    {
+        public int? home_score { get; set; } // 6
+        public int? away_score { get; set; } // 10
+        public string? type { get; set; } // "regular_period"
+        public int? number { get; set; } // 1
+    }
 }
