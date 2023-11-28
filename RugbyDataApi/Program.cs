cc43b0747db839c2af7c3ef34b288c0f0bcde040
MossyPower <10609394@mydbs.ie>
2023-11-28 20:48:09 +0000

    Scaffold Controller

--- RugbyDataApi/Program.cs
+++ RugbyDataApi/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using RugbyDataApi.Models;
+using RugbyDataApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration
+    .GetConnectionString("RugbyDataApiDBConnection") ?? throw new InvalidOperationException("Connection string'RugbyDataApiDBConnection' not found.");
+
+builder.Services.AddDbContext<RugbyDataDbContext>(options =>
+    options.UseSqlite(connectionString));
 
 // Add services to the container.
 
@@ -15,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
