3643cd6eb7b59687620feb6f90e739744d87c457
MossyPower <10609394@mydbs.ie>
2024-01-15 08:46:09 +0000

    Add Create Link to Seasons Index

--- MvcRugby/obj/Debug/net7.0/MvcRugby.AssemblyInfo.cs
+++ MvcRugby/obj/Debug/net7.0/MvcRugby.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcRugby")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+456ab236ac6dc94783f4773dafd635806e842eaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcRugby")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcRugby")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
